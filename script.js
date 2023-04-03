13dce7fb402e4989d19703c884dc08a7a75b080c
Aaron Keohane <aakeohane@gmail.com>
2023-04-03 12:32:19 -0700

    possible setup for mutationObserver but might not need to

--- script.js
+++ script.js
@@ -3,45 +3,76 @@ const groupTwo = document.getElementById("list2")
 const groupThree = document.getElementById("list3")
 const groupFour = document.getElementById("list4")
 
+const lockerNumber = document.getElementById("locker")
+const roomType = document.getElementById("roomType")
+
 const button = document.getElementById("button")
 
+// const config = {
+//   childList: true
+// }
+
+// // Callback function to execute when mutations are observed
+// const callback = (mutationList, observer) => {
+//   for (const mutation of mutationList) {
+//     if (mutation.type === "childList") {
+//       console.log("A child node has been added or removed.");
+//     } else if (mutation.type === "attributes") {
+//       console.log(`The ${mutation.attributeName} attribute was modified.`);
+//     }
+//   }
+// };
+
+// // Create an observer instance linked to the callback function
+// const observer = new MutationObserver(callback);
+
+// // Start observing the target node for configured mutations
+// observer.observe(groupOne, config);
+
 const listOne = [
-  98, 17, 12, 19, 7, 6
 ]
 
 const listTwo = [
-  100, 10, 2, 33, 4, 19, 45
 ]
 
 const listThree = [
-  2, 7, 8
 ]
 
 const listFour = [
-  99, 1
 ]
 
 button.addEventListener('click', () => {
   const newElement = document.createElement('div')
-  newElement.value = 8
-  newElement.innerText = 8
+  newElement.innerText = lockerNumber.value
   newElement.className = "listItems"
-  listThree.push(newElement.value)
-  groupThree.appendChild(newElement)
-  console.log(listThree)
+  let list
+  switch(roomType.value) {
+    case "basic":
+      list = groupTwo
+      break;
+    case "suite":
+      list = groupThree
+      break;
+    case "lux":
+      list = groupFour
+      break;
+    default:
+      list = groupOne
+  }
+  list.appendChild(newElement)
 });
 
 
-function makeList(location, list) {
-  list.forEach((item) => {
-    let div = document.createElement("div");
-    div.innerText = item
-    div.className = "listItems"
-    location.appendChild(div)
-  })
-}
+// function makeList(location, list) {
+//   list.forEach((item) => {
+//     let div = document.createElement("div");
+//     div.innerText = item
+//     div.className = "listItems"
+//     location.appendChild(div)
+//   })
+// }
 
-makeList(groupOne, listOne)
-makeList(groupTwo, listTwo)
-makeList(groupThree, listThree)
-makeList(groupFour, listFour)
+// makeList(groupOne, listOne)
+// makeList(groupTwo, listTwo)
+// makeList(groupThree, listThree)
+// makeList(groupFour, listFour)
