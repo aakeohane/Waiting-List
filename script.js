1e0834ad0d9b86af7678bfd876905ab27a0de746
Aaron Keohane <aakeohane@gmail.com>
2023-04-04 11:24:04 -0700

    dont need mutation, add removeChild/list capability

--- script.js
+++ script.js
@@ -8,41 +8,8 @@ const roomType = document.getElementById("roomType")
 
 const button = document.getElementById("button")
 
-// const config = {
-//   childList: true
-// }
-
-// // Callback function to execute when mutations are observed
-// const callback = (mutationList, observer) => {
-//   for (const mutation of mutationList) {
-//     if (mutation.type === "childList") {
-//       console.log("A child node has been added or removed.");
-//     } else if (mutation.type === "attributes") {
-//       console.log(`The ${mutation.attributeName} attribute was modified.`);
-//     }
-//   }
-// };
-
-// // Create an observer instance linked to the callback function
-// const observer = new MutationObserver(callback);
-
-// // Start observing the target node for configured mutations
-// observer.observe(groupOne, config);
-
-const listOne = [
-]
-
-const listTwo = [
-]
-
-const listThree = [
-]
-
-const listFour = [
-]
-
 button.addEventListener('click', () => {
-  const newElement = document.createElement('div')
+  const newElement = document.createElement('li')
   newElement.innerText = lockerNumber.value
   newElement.className = "listItems"
   let list
@@ -60,19 +27,10 @@ button.addEventListener('click', () => {
       list = groupOne
   }
   list.appendChild(newElement)
+  const listItems = document.querySelectorAll("li")
+  listItems.forEach((item) => {
+    item.onclick = function(e) {
+      this.parentNode.removeChild(this)
+    }
+  })
 });
-
-
-// function makeList(location, list) {
-//   list.forEach((item) => {
-//     let div = document.createElement("div");
-//     div.innerText = item
-//     div.className = "listItems"
-//     location.appendChild(div)
-//   })
-// }
-
-// makeList(groupOne, listOne)
-// makeList(groupTwo, listTwo)
-// makeList(groupThree, listThree)
-// makeList(groupFour, listFour)
