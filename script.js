0ee00f984e926f4f8d1af13ce4f19d89e0b2e827
Aaron Keohane <aakeohane@gmail.com>
2023-04-05 00:04:35 -0700

    clean up createListItem function and added remove list item function along with addition of all list cloneNode

--- script.js
+++ script.js
@@ -2,35 +2,101 @@ const groupOne = document.getElementById("list1")
 const groupTwo = document.getElementById("list2")
 const groupThree = document.getElementById("list3")
 const groupFour = document.getElementById("list4")
+const groupFive = document.getElementById("list5")
+
+const allGroups = document.getElementsByClassName("roomList")
 
 const lockerNumber = document.getElementById("locker")
 const roomType = document.getElementById("roomType")
 
 const button = document.getElementById("button")
 
+const contextMenu = document.getElementById("contextMenu")
+
+const contextMenuRemove = document.getElementById("delete")
+
 button.addEventListener('click', () => {
+  createListItem()
+});
+
+function createListItem() {
   const newElement = document.createElement('li')
   newElement.innerText = lockerNumber.value
   newElement.className = "listItems"
+
+  const deleteIcon = document.createElement('img');
+  deleteIcon.setAttribute('class', 'remove-icon');
+  deleteIcon.setAttribute('src', 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/375261/System_Delete.ico');
+
   let list
   switch(roomType.value) {
-    case "basic":
+    case "non-tv":
+      list = groupOne
+      break;
+    case "reg-tv":
       list = groupTwo
       break;
-    case "suite":
+    case "large-tv":
       list = groupThree
       break;
-    case "lux":
+    case "suite":
       list = groupFour
       break;
+    case "deluxe":
+      list = groupFive
+      break;
     default:
-      list = groupOne
+      list = "all"
   }
-  list.appendChild(newElement)
-  const listItems = document.querySelectorAll("li")
-  listItems.forEach((item) => {
-    item.onclick = function(e) {
-      this.parentNode.removeChild(this)
+  deleteIcon.addEventListener("click", function(e) {
+    removeLocker(e)
+  }, false)
+  newElement.appendChild(deleteIcon)
+  if (list === "all") {
+    Array.from(allGroups).forEach(element => {
+      
+      const newEl = element.appendChild(newElement.cloneNode(true))
+      console.log(newEl)
+    })
+    const clonedGroup = document.querySelectorAll(".remove-icon")
+    // I have to delegate event listener individually because cloning nodes does not bring event listeners with it
+    for (const clone of clonedGroup) {
+      clone.addEventListener("click", removeLocker)
     }
-  })
-});
+  } else 
+  list.appendChild(newElement)
+}
+
+function removeLocker(e) {
+  console.log("clicked")
+  var el = e.target;
+  var elListItem= el.parentNode;
+  elFullList = elListItem.parentNode;
+  elFullList.removeChild(elListItem);
+} 
+
+
+// function onDragStart(event) {
+//   event.dataTransfer.setData('text/plain', event.target.id)
+//   event.currentTarget.style.backgroundColor = 'yellow'
+// }
+// function onDragOver(event) {
+//   event.preventDefault();
+// }
+// function onDrop(event) {
+//   const id = event.dataTransfer.getData('text')
+//   const draggableEl = document.getElementById(id)
+//   const dropzone = event.target
+//   dropzone.appendChild(draggableEl)
+//   event.dataTransfer.clearData()
+// }
+
+
+
+
+
+      
+
+
+
+
